9ec2556c7f96c79f87d5884e18620c0a37b5dac8
baiyun-11 <84278744+baiyun-11@users.noreply.github.com>
2021-06-25 15:05:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+#=================================================================
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>            
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
+# This is free software, licensed under the MIT License.         
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+#=================================================================
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+# Modify WiFi ON
+#sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# Modify default SSID
+#sed -i 's/ssid=OpenWrt/ssid=lantian/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh

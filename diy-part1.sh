7b0ff952d289735f53c503c6302b6642db20c77e
baiyun-11 <84278744+baiyun-11@users.noreply.github.com>
2021-07-13 20:53:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,7 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git luci https://github.com/immortalwrt/luci.git;openwrt-18.06

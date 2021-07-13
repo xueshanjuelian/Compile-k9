d3fd14f5b95d5b8fb4eb1bc173a74095bb05b8df
baiyun-11 <84278744+baiyun-11@users.noreply.github.com>
2021-07-13 10:00:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git luci https://github.com/immortalwrt/luci.git;openwrt-18.06
+#sed -i '$a src-git luci https://github.com/immortalwrt/luci.git;openwrt-18.06
 #sed -i '$a src-git garypang13 https://github.com/garypang13/openwrt-packages.git' feeds.conf.default
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git 281677160 https://github.com/281677160/openwrt-package.git' feeds.conf.default

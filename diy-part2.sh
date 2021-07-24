5ece9eec6cff9cdf922e213c5a6e0061602d81e4
baiyunxue <84278744+baiyunxue@users.noreply.github.com>
2021-07-24 21:01:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,9 +18,9 @@ sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac802
 #sed -i 's/ssid=OpenWrt/ssid=lantian/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 #Add ddnsto
-svn co https://github.com/281677160/openwrt-package/branches/openwrt-21.02/package/luci-app-ddnsto/luci/luci-app-ddnsto package/feeds/luci/luci-app-ddnsto
-svn co https://github.com/281677160/openwrt-package/branches/openwrt-21.02/package/luci-app-ddnsto/luci/luci-app-ddnsto feeds/luci/applications/luci/luci-app-ddnsto
-svn co https://github.com/281677160/openwrt-package/branches/openwrt-21.02/package/luci-app-ddnsto/network/services/ddnsto package/network/services/ddnsto
+#svn co https://github.com/281677160/openwrt-package/branches/openwrt-21.02/package/luci-app-ddnsto/luci/luci-app-ddnsto package/feeds/luci/luci-app-ddnsto
+#svn co https://github.com/281677160/openwrt-package/branches/openwrt-21.02/package/luci-app-ddnsto/luci/luci-app-ddnsto feeds/luci/applications/luci/luci-app-ddnsto
+#svn co https://github.com/281677160/openwrt-package/branches/openwrt-21.02/package/luci-app-ddnsto/network/services/ddnsto package/network/services/ddnsto
 
 #Add luci-app-zerotier
 #svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-zerotier package/feeds/luci/luci-app-zerotier

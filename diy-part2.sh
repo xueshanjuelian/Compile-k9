ecbf9f2208449c8345e73d93220622f91d5eef76
baiyunxue <84278744+baiyunxue@users.noreply.github.com>
2021-09-09 16:01:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,12 +18,16 @@ sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac802
 #sed -i 's/ssid=OpenWrt/ssid=lantian/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 自定义app
-git clone monokoo https://github.com/monokoo/luci-app-arpbind.git package/DIY/luci-app-arpbind
-git clone awesome-openwrt https://github.com/awesome-openwrt/luci-app-autoreboot.git package/DIY/luci-app-autoreboot
+git clone monokoo https://github.com/monokoo/luci-app-arpbind.git package/DIY/luci-app-arpbind   #IP绑定
+git clone awesome-openwrt https://github.com/awesome-openwrt/luci-app-autoreboot.git package/DIY/luci-app-autoreboot  #计划重启
 git clone mchome https://github.com/mchome/luci-app-vlmcsd.git package/DIY/luci-app-vlmcsd   #KSM服务app
 git clone mchome https://github.com/mchome/openwrt-vlmcsd.git package/DIY/vlmcsd   #KSM服务
+git clone monokoo https://github.com/monokoo/luci-app-transmission.git package/DIY/luci-app-transmission   #transmission下载
+git clone pymumu https://github.com/pymumu/luci-app-smartdns.git package/DIY/luci-app-smartdns  #smartdns加速app
+git clone pymumu https://github.com/pymumu/openwrt-smartdns.git package/DIY/smartdns   #smartdns加速
 svn co https://github.com/animefansxj/luci-app-vsftpd/trunk/Source/op-dir/feeds/packages/net/vsftpd package/DIY/vsftpd   #vsftpd共享
 svn co https://github.com/animefansxj/luci-app-vsftpd/trunk/Source/luci-dir/applications/luci-vsftpd package/DIY/luci-app-vsftpd   #vsftpd共享app
+
 
 # 自定义theme主题
 git clone Leo-Jo-My https://github.com/Leo-Jo-My/luci-theme-opentomato.git package/DIY/luci-theme-opentomato

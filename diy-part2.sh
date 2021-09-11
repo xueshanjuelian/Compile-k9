cc4898de58e1c38fce08570acd464ca840f28de3
baiyunxue <84278744+baiyunxue@users.noreply.github.com>
2021-09-11 16:58:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -30,8 +30,8 @@ svn co https://github.com/animefansxj/luci-app-vsftpd/trunk/Source/op-dir/feeds/
 svn co https://github.com/animefansxj/luci-app-vsftpd/trunk/Source/luci-dir/applications/luci-vsftpd package/DIY/luci-app-vsftpd   #vsftpd共享app
 
 # 自定义theme主题
-git clone Leo-Jo-My https://github.com/Leo-Jo-My/luci-theme-opentomato.git package/DIY/luci-theme-opentomato
-git clone Leo-Jo-My https://github.com/Leo-Jo-My/luci-theme-argon-dark-mod.git package/DIY/luci-theme-argon-dark-mod
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git package/DIY/luci-theme-opentomato
+git clone https://github.com/Leo-Jo-My/luci-theme-argon-dark-mod.git package/DIY/luci-theme-argon-dark-mod
 
 #自定义软件包
 #git clone -b 19.07 https://github.com/kenzok8/openwrt-packages package/kenzok8

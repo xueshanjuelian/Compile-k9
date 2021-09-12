aabdab2a31b6ef4f6d4d87d90f9daa618f69baa2
baiyunxue <84278744+baiyunxue@users.noreply.github.com>
2021-09-12 21:07:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,16 +18,24 @@ sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac802
 #sed -i 's/ssid=OpenWrt/ssid=lantian/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 自定义app
-svn co https://github.com/gw826943555/openwrt-vlmcsd/trunk/luci-app-vlmcsd package/DIY/luci-app-vlmcsd   #KSM服务app
-svn co https://github.com/gw826943555/openwrt-vlmcsd/trunk/vlmcsd package/DIY/vlmcsd   #KSM服务
-svn co https://github.com/gw826943555/openwrt-vsftpd/trunk/vsftpd package/DIY/vsftpd   #vsftpd共享
-svn co https://github.com/gw826943555/openwrt-vsftpd/trunk/luci-app-vsftpd package/DIY/luci-app-vsftpd   #vsftpd共享app
-svn co https://github.com/coolsnowwolf/packages/trunk/net/zerotier package/DIY/zerotier
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-zerotier package/DIY/luci-app-zerotier
+svn co https://github.com/zxlhhyccc/bf-package/trunk/lean/luci-app-vsftpd package/false/luci-app-vsftpd
+svn co https://github.com/zxlhhyccc/bf-package/trunk/lean/vsftpd-alt package/false/vsftpd-alt
+svn co https://github.com/zxlhhyccc/bf-package/trunk/lean/luci-app-vlmcsd package/false/luci-app-vlmcsd
+svn co https://github.com/zxlhhyccc/bf-package/trunk/lean/vlmcsd package/false/vlmcsd
+svn co https://github.com/zxlhhyccc/bf-package/trunk/lean/luci-app-ramfree package/false/luci-app-ramfree
+svn co https://github.com/zxlhhyccc/bf-package/trunk/lean/luci-app-autoreboot package/false/luci-app-autoreboot
+svn co https://github.com/zxlhhyccc/bf-package/trunk/lean/luci-app-arpbind package/false/luci-app-arpbind
+svn co https://github.com/zxlhhyccc/bf-package/trunk/lean/luci-app-accesscontrol package/false/luci-app-accesscontrol
+#svn co https://github.com/gw826943555/openwrt-vlmcsd/trunk/luci-app-vlmcsd package/DIY/luci-app-vlmcsd   #KSM服务app
+#svn co https://github.com/gw826943555/openwrt-vlmcsd/trunk/vlmcsd package/DIY/vlmcsd   #KSM服务
+#svn co https://github.com/gw826943555/openwrt-vsftpd/trunk/vsftpd package/DIY/vsftpd   #vsftpd共享
+#svn co https://github.com/gw826943555/openwrt-vsftpd/trunk/luci-app-vsftpd package/DIY/luci-app-vsftpd   #vsftpd共享app
+#svn co https://github.com/coolsnowwolf/packages/trunk/net/zerotier package/DIY/zerotier
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-zerotier package/DIY/luci-app-zerotier
 
 # 自定义theme主题
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git package/DIY/luci-theme-opentomato
-git clone https://github.com/Leo-Jo-My/luci-theme-argon-dark-mod.git package/DIY/luci-theme-argon-dark-mod
+#git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git package/DIY/luci-theme-opentomato
+#git clone https://github.com/Leo-Jo-My/luci-theme-argon-dark-mod.git package/DIY/luci-theme-argon-dark-mod
 
 #自定义软件包
 #git clone -b 19.07 https://github.com/kenzok8/openwrt-packages package/kenzok8

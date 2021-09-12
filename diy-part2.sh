61f638c9ce0318c9bd03f741e38ac9f3b8a0151a
baiyunxue <84278744+baiyunxue@users.noreply.github.com>
2021-09-12 17:03:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,15 +18,12 @@ sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac802
 #sed -i 's/ssid=OpenWrt/ssid=lantian/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 自定义app
-git clone https://github.com/monokoo/luci-app-arpbind.git package/DIY/luci-app-arpbind   #IP绑定
-git clone https://github.com/awesome-openwrt/app-autoreboot.git package/DIY/luci-app-autoreboot  #计划重启
-git clone https://github.com/awesome-openwrt/app-ramfree.git package/DIY/luci-app-ramfree   #内存释放
-svn co https://github.com/zxlhhyccc/bf-package/trunk/lean/luci-app-accesscontrol package/DIY/luci-app-accesscontrol   #上网时间控制
-svn co https://github.com/zxlhhyccc/bf-package/trunk/lienol/luci-app-timecontrol package/DIY/luci-app-timecontrol
 svn co https://github.com/gw826943555/openwrt-vlmcsd/trunk/luci-app-vlmcsd package/DIY/luci-app-vlmcsd   #KSM服务app
 svn co https://github.com/gw826943555/openwrt-vlmcsd/trunk/vlmcsd package/DIY/vlmcsd   #KSM服务
 svn co https://github.com/gw826943555/openwrt-vsftpd/trunk/vsftpd package/DIY/vsftpd   #vsftpd共享
 svn co https://github.com/gw826943555/openwrt-vsftpd/trunk/luci-app-vsftpd package/DIY/luci-app-vsftpd   #vsftpd共享app
+svn co https://github.com/coolsnowwolf/packages/trunk/net/zerotier package/DIY/zerotier
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-zerotier package/DIY/luci-app-zerotier
 
 # 自定义theme主题
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git package/DIY/luci-theme-opentomato

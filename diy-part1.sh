60cc62603a152f2120057b860dcddcfaf77055ab
xueshanjuelian <xueshanjuelian@163.com>
2026-06-11 08:44:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 #sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
-#echo "src-git kiddin9 https://github.com/kiddin9/op-packages.git;main" >> ./feeds.conf.default
+echo "src-git kiddin9 https://github.com/kiddin9/op-packages.git;main" >> ./feeds.conf.default
 #echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 #echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
 #echo "src-git usbmodem https://github.com/Azq2/openwrt-usbmodem" >> ./feeds.conf.default
